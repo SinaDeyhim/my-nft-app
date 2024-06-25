4f81ce0b83bec9082144164f4cc1008fae3fc44c
Sina Deyhim <sina.deyhim@gmail.com>
2024-06-25 12:21:38 -0400

    layout fix

--- src/providers/NFTProvider.tsx
+++ src/providers/NFTProvider.tsx
@@ -1,10 +1,4 @@
-import React, {
-  ReactNode,
-  createContext,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { NFT } from "@/clients/stargaze";
 
 interface NFTPosition {
@@ -30,26 +24,44 @@ export const useNFTPositionContext = () => {
   return context;
 };
 
+// Function to calculate initial positions based on available width
+const calculateInitialPositions = (nfts: NFT[]) => {
+  const containerWidth = 1000; // Example: Replace with actual width calculation
+  const maxNftsPerRow = Math.floor(containerWidth / 120); // Assuming each NFT is 120px wide
+
+  let currentTop = 0;
+  let currentLeft = 0;
+  let n = 0;
+
+  const initialPositions = nfts.reduce((acc, nft, index) => {
+    if (index > 0 && index % maxNftsPerRow === 0) {
+      currentTop += 150; // Adjust top value after reaching max NFTs per row
+      currentLeft = 0;
+      n++;
+    }
+
+    acc[nft.id] = { left: currentLeft * 120, top: currentTop };
+    currentLeft++;
+
+    return acc;
+  }, {} as { [key: string]: NFTPosition });
+
+  return initialPositions;
+};
+
 export const NFTProvider: React.FC<{
   nfts: NFT[];
-  children: ReactNode;
-}> = ({ nfts, children }: { nfts: NFT[]; children: ReactNode }) => {
+  children: React.ReactNode;
+}> = ({ nfts, children }: { nfts: NFT[]; children: React.ReactNode }) => {
   const [positions, setPositions] = useState<{ [key: string]: NFTPosition }>(
     () => {
-      return nfts.reduce((acc, nft, index) => {
-        acc[nft.id] = { left: index * 120, top: 0 }; // Initial positions
-        return acc;
-      }, {});
+      return calculateInitialPositions(nfts);
     }
   );
 
   useEffect(() => {
-    // Initialize positions when initialNFTs change
     if (nfts && nfts.length > 0) {
-      const initialPositions = nfts.reduce((acc, nft, index) => {
-        acc[nft.id] = { left: index * 120, top: 0 }; // Assuming 120px width for each NFT
-        return acc;
-      }, {});
+      const initialPositions = calculateInitialPositions(nfts);
       setPositions(initialPositions);
     }
   }, [nfts]);
